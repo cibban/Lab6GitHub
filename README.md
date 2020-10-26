f7442660ddb4e814b7599253bcb541642f8c3da3
cibban <cibban@users.noreply.github.com>
2020-10-26 17:12:26 +0100

    no message

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lab6GitHub
+Labb 6
+Alexandra Niklasson och Patrik Sjöstrand
--- src/TestMainFunctions.java
+++ src/TestMainFunctions.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
